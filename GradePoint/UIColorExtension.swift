0cabf067efb36078ab24ad1ed169e582762ef161
Luis Padron <luispadronedu@gmail.com>
2016-10-12 21:33:16 -0400

    add color theme as UIColor Extension

---
+++ GradePoint/UIColorExtension.swift
@@ -0,0 +1,26 @@
+//
+//  UIColorExtension.swift
+//  GradePoint
+//
+//  Created by Luis Padron on 10/12/16.
+//  Copyright © 2016 Luis Padron. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    // MARK: - Main Theme
+    
+    static let mainDark = UIColor(red: 68/255, green: 68/255, blue: 79/255, alpha: 1.0) /* #44444f */
+    static let highlight = UIColor(red: 111/255, green: 190/255, blue: 217/255, alpha: 1.0) /* #6fbed9 */
+    static let unselected = UIColor(red: 199/255, green: 199/255, blue: 205/255, alpha: 1.0) /* #c7c7cd */
+    static let mainText = UIColor.white
+    static let textMuted = UIColor(red: 155/255, green: 155/255, blue: 155/255, alpha: 1.0) /* #9b9b9b */
+    
+    // MARK: - TableView & Cells Theme
+    
+    static let tableViewHeader = UIColor(red: 100/255, green: 100/255, blue: 112/255, alpha: 1.0) /* #646470 */
+    static let tableViewSeperator = UIColor(red: 78/255, green: 81/255, blue: 94/255, alpha: 1.0) /* #4e515e */
+    
+}
