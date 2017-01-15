066ee95691118d11e63d203992949c940c2bf242
Luis Padron <Luis@luispadron.com>
2017-01-14 20:24:17 -0500

    Hook up the ClassDetailTableViewController
    - ClassDetailTableViewController now refreshes on creation and shows cells
    - Configure AssignmentTableViewCell

--- GradePoint/AssignmentTableViewCell.swift
+++ GradePoint/AssignmentTableViewCell.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class AssignmentTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        nameLabel.textColor = UIColor.lightText
+        scoreLabel.textColor = UIColor.mutedText
+        dateLabel.textColor = UIColor.mutedText
     }
 
 }
--- GradePoint/ClassDetailTableViewController.swift
+++ GradePoint/ClassDetailTableViewController.swift
@@ -14,6 +14,9 @@ class ClassDetailTableViewController: UITableViewController {
 
     // MARK: - Properties
     
+    var realm = try! Realm()
+    var notificationToken: NotificationToken?
+    
     @IBOutlet var progressRing: UICircularProgressRingView!
     
     var detailItem: Class? {
@@ -23,13 +26,16 @@ class ClassDetailTableViewController: UITableViewController {
         }
     }
     
-
     // MARK: - Overrides
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Configure the view for load
         configureView()
+        
+        notificationToken = realm.addNotificationBlock {_,_ in 
+            self.tableView.reloadData()
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -38,11 +44,82 @@ class ClassDetailTableViewController: UITableViewController {
         self.tableView.separatorColor = UIColor.tableViewSeperator
     }
     
-    
     // MARK: - TableView Methods
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return detailItem?.rubrics.count ?? 0
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let rubrics = detailItem?.rubrics, let parentClass = detailItem else {
+            print("Could not get number rubrics for tableView")
+            return 0
+        }
+        
+        let rubricForSection = rubrics[section]
+        let assignmentsForSection = parentClass.assignments.filter("associatedRubric =  %@", rubricForSection)
+        
+        return assignmentsForSection.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        guard let rubrics = detailItem?.rubrics, let parentClass = detailItem else {
+            print("Could not get number rubrics for tableView")
+            return 0
+        }
+        
+        let rubricForSection = rubrics[section]
+        let assignmentForSection = parentClass.assignments.filter("associatedRubric = %@", rubricForSection)
+        
+        return assignmentForSection.count > 0 ? 44 : 0
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let rubrics = detailItem?.rubrics else {
+            print("Error getting rubrics for header view")
+            return nil
+        }
+        // Create the correct headerView for the section
+        let rubricForSection = rubrics[section]
+        
+        let mainView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: 44))
+        mainView.backgroundColor = UIColor.tableViewHeader
+        
+        let label = UILabel(frame: CGRect(x: 20, y: 0, width: mainView.bounds.size.width, height: 44))
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.textColor = UIColor.unselected
+        label.backgroundColor = UIColor.tableViewHeader
+        mainView.addSubview(label)
+        
+        // Set the correct label text
+        label.text = "\(rubricForSection.name)"
+        
+        return mainView
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let rubrics = detailItem?.rubrics, let parentClass = detailItem else {
+            print("Error couldn't get rubrics or parentclass in cellForRow")
+            return UITableViewCell()
+        }
+        
+        let rubricForSection = rubrics[indexPath.section]
+        let assignment = parentClass.assignments
+                                    .filter("associatedRubric = %@", rubricForSection)
+                                    .sorted(byProperty: "date", ascending: false)[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "assignmentTableViewCell", for: indexPath) as! AssignmentTableViewCell
+        
+        cell.nameLabel.text = assignment.name
+        cell.scoreLabel.text = "Score: \(assignment.score)"
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        cell.dateLabel.text = "Date: " + formatter.string(from: assignment.date)
+        
+        return cell
+    }
+    
     
     // MARK: - Segues
     
@@ -80,6 +157,10 @@ class ClassDetailTableViewController: UITableViewController {
         }
         
         super.viewWillLayoutSubviews()
+    }
+    
+    deinit {
+        self.notificationToken?.stop()
     }
 }
 
