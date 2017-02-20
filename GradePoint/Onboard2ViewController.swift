746db62c2a2d5c897cd6846f0ed8b547c20b1555
Luis Padron <Luis@luispadron.com>
2017-02-19 23:30:19 -0500

    Add onboarding view 2
    - Add animations and views for onboarding view #2

---
+++ GradePoint/Onboard2ViewController.swift
@@ -0,0 +1,140 @@
+//
+//  Onboard2ViewController.swift
+//  GradePoint
+//
+//  Created by Luis Padron on 2/19/17.
+//  Copyright © 2017 Luis Padron. All rights reserved.
+//
+
+import UIKit
+
+class Onboard2ViewController: UIViewController {
+
+    // MARK: Views
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var classAddExample: UIImageView!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var rubricStackView: UIStackView!
+    @IBOutlet weak var rubricView1: UIImageView!
+    @IBOutlet weak var rubricView2: UIImageView!
+    @IBOutlet weak var rubricView3: UIImageView!
+    @IBOutlet weak var rubricView4: UIImageView!
+    @IBOutlet weak var thirdLabel: UILabel!
+    @IBOutlet weak var swipeLabel: UILabel!
+    
+    private var hasAnimated = false
+    private var yForRubricView2: CGFloat = 0.0
+    private var yForRubricView3: CGFloat = 0.0
+    private var yForRubricView4: CGFloat = 0.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set opacity for views
+        firstLabel.alpha = 0.0
+        secondLabel.alpha = 0.0
+        thirdLabel.alpha = 0.0
+        swipeLabel.alpha = 0.0
+        classAddExample.alpha = 0.0
+        rubricView1.alpha = 0.0
+        rubricView2.alpha = 0.0
+        rubricView3.alpha = 0.0
+        rubricView4.alpha = 0.0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if !hasAnimated { self.animateViews() }
+        self.hasAnimated = true
+    }
+    
+
+    func animateViews() {
+        // Set the old y origins for the rubric views
+        yForRubricView2 = self.rubricView2.center.y
+        yForRubricView3 = self.rubricView3.center.y
+        yForRubricView4 = self.rubricView4.center.y
+        // Now 'collapse' all the rubrics onto rubric 1
+        self.rubricView2.center.y = self.rubricView1.center.y
+        self.rubricView3.center.y = self.rubricView1.center.y
+        self.rubricView4.center.y = self.rubricView1.center.y
+        
+        
+        let duration: TimeInterval = 1.5
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: [], animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/3, animations: {
+                self.firstLabel.alpha = 1.0
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 1/3, relativeDuration: 1/3, animations: {
+                self.classAddExample.alpha = 1.0
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 2/3, relativeDuration: 1/3, animations: {
+                self.secondLabel.alpha = 1.0
+            })
+            
+            
+        }) { (finished) in
+            if finished { self.animateRubricViews() }
+        }
+    }
+    
+    func animateRubricViews() {
+        let duration: TimeInterval = 3.0
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: [], animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/4, animations: { 
+                self.rubricView1.alpha = 1.0
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 1/4, relativeDuration: 1/4, animations: {
+                self.rubricView2.alpha = 1.0
+                self.rubricView2.center.y = self.yForRubricView2
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 2/4, relativeDuration: 1/4, animations: {
+                self.rubricView3.alpha = 1.0
+                self.rubricView3.center.y = self.yForRubricView3
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 3/4, relativeDuration: 1/4, animations: {
+                self.rubricView4.alpha = 1.0
+                self.rubricView4.center.y = self.yForRubricView4
+            })
+            
+        }) { (finished) in
+            // Finally animate the last label and the swipe label
+            if finished {
+                UIView.animateKeyframes(withDuration: 1.0, delay: 0.0, options: [], animations: { 
+                    UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/2, animations: { 
+                        self.thirdLabel.alpha = 1.0
+                    })
+                    
+                    UIView.addKeyframe(withRelativeStartTime: 1/2, relativeDuration: 1/2, animations: {
+                        self.swipeLabel.alpha = 1.0
+                    })
+                    
+                }, completion: { (finished) in
+                    // Keep animating the swipe label
+                    if finished {
+                        UIView.animateKeyframes(withDuration: 1.5, delay: 0.0, options: .repeat, animations: { 
+                            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/2, animations: { 
+                                self.swipeLabel.alpha = 0.5
+                            })
+                            
+                            UIView.addKeyframe(withRelativeStartTime: 1/2, relativeDuration: 1/2, animations: { 
+                                self.swipeLabel.alpha = 1.0
+                            })
+                        }, completion: nil)
+                    }
+                })
+            }
+        }
+    }
+    
+}
