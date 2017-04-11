7bebb67d1a7f4ca55bd04cd1066937e85d0a95f4
Luis Padron <luispadronn@gmail.com>
2017-04-11 00:14:36 -0400

    Add animations to new onboarding view

--- GradePoint/Onboard3ViewController.swift
+++ GradePoint/Onboard3ViewController.swift
@@ -15,13 +15,50 @@ class Onboard3ViewController: UIViewController {
     @IBOutlet weak var schoolTypeSegmentControl: UISegmentedControl!
     @IBOutlet weak var gradingTypeLabel: UILabel!
     @IBOutlet weak var gradingTypeSegementControl: UISegmentedControl!
+    @IBOutlet weak var swipeLabel: UILabel!
     
+    /// Reference to the parent controller
     weak var pageController: OnboardPageViewController?
+    /// Determines whether we have previously animated the grading type segment and label, if so, don't animate again
+    var hasAnimatedGradingType = false
+    /// Determines whether we have started the animation for the swipe label, if so, dont start it again
+    var hasStartedSwipeAnimation = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // UI Set up, make all alphas 0, since I suck at animations and alpha animations are easy
+        titleLabel.alpha = 0.0
+        schoolTypeLabel.alpha = 0.0
+        schoolTypeSegmentControl.alpha = 0.0
+        gradingTypeLabel.alpha = 0.0
+        gradingTypeSegementControl.alpha = 0.0
+        swipeLabel.alpha = 0.0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Animate the views
+        self.animateViews()
+    }
+    
+    func animateViews() {
+        // First animate the title, school type label and segment control
+        // When user selects a school type, animate the grading type segment
+        UIView.animateKeyframes(withDuration: 1.5, delay: 0.0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/3, animations: { 
+                self.titleLabel.alpha = 1.0
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 1/3, relativeDuration: 1/3, animations: {
+                self.schoolTypeLabel.alpha = 1.0
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 2/3, relativeDuration: 1/3, animations: {
+                self.schoolTypeSegmentControl.alpha = 1.0
+            })
+            
+        }, completion: nil)
     }
 
     // MARK: Actions
@@ -29,12 +66,40 @@ class Onboard3ViewController: UIViewController {
     @IBAction func schoolTypeChanged(_ sender: UISegmentedControl) {
         pageController?.dataSource = nil
         pageController?.dataSource = pageController!
+        
+        // If we haven't animated before, then animate the grading type label and control in now
+        if !hasAnimatedGradingType {
+            UIView.animateKeyframes(withDuration: 1.0, delay: 0.0, options: [], animations: {
+                UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/2, animations: {
+                    self.gradingTypeLabel.alpha = 1.0
+                })
+                
+                UIView.addKeyframe(withRelativeStartTime: 1/2, relativeDuration: 1/2, animations: { 
+                    self.gradingTypeSegementControl.alpha = 1.0
+                })
+            }, completion: nil)
+        }
     }
     
     
     @IBAction func gradingTypeChanged(_ sender: UISegmentedControl) {
         pageController?.dataSource = nil
         pageController?.dataSource = pageController!
+        
+        // If we haven't animated the swipe label, do so now
+        if !hasStartedSwipeAnimation {
+            self.swipeLabel.alpha = 1.0
+            UIView.animateKeyframes(withDuration: 1.5, delay: 0.0, options: .repeat, animations: {
+                UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/2, animations: {
+                    self.swipeLabel.alpha = 0.5
+                })
+                
+                UIView.addKeyframe(withRelativeStartTime: 1/2, relativeDuration: 1/2, animations: {
+                    self.swipeLabel.alpha = 1.0
+                })
+                
+            }, completion: nil)
+        }
     }
     
     // MARK: Helpers
