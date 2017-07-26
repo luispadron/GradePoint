d897c8266d144e047b109e51b9710b1d0a71f7cf
Luis Padron <luispadronn@gmail.com>
2017-07-26 18:18:47 -0400

    Add subtle drop shadow to calculator views

--- GradePoint/CalculatorsViewController.swift
+++ GradePoint/CalculatorsViewController.swift
@@ -12,6 +12,8 @@ import RealmSwift
 
 class CalculatorsViewController: UIViewController {
 
+    @IBOutlet weak var gpaCalculatorView: UIView!
+    @IBOutlet weak var examCalculatorView: UIView!
     @IBOutlet weak var gpaRing: UICircularProgressRingView!
     @IBOutlet weak var lastCalculationLabel: UILabel!
     
@@ -23,9 +25,21 @@ class CalculatorsViewController: UIViewController {
         super.viewWillAppear(animated)
 
         // Set up the UI
-        
         self.view.backgroundColor = UIColor.background
         self.gpaRing.font = UIFont.systemFont(ofSize: self.gpaRing.frame.width/7.0)
+
+        // Add drop shadow to calculator views
+        gpaCalculatorView.layer.cornerRadius = 10
+        gpaCalculatorView.layer.shadowColor = UIColor.black.cgColor
+        gpaCalculatorView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        gpaCalculatorView.layer.shadowOpacity = 0.4
+        gpaCalculatorView.layer.shadowRadius = 5.0
+
+        examCalculatorView.layer.cornerRadius = 10
+        examCalculatorView.layer.shadowColor = UIColor.black.cgColor
+        examCalculatorView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        examCalculatorView.layer.shadowOpacity = 0.4
+        examCalculatorView.layer.shadowRadius = 5.0
 
         
         // Set the progress ring and label
